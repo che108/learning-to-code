5080e4f7782befebfd6820dc9bd562458d50ebf6
che108 <chanteventer@gmail.com>
2017-04-06 20:48:35 +0200

    Add 'var' to pizza1, pizza2

--- max.js
+++ max.js
@@ -10,8 +10,8 @@ function computeMax (a,b) {
   }
 }
 
-pizza1 = 60;
-pizza2 = 100;
+var pizza1 = 60;
+var pizza2 = 100;
 var priceOfPizza = computeMax(pizza1, pizza2);
 var otherStuff = 20;
 var total = priceOfPizza + otherStuff;
